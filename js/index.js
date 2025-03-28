9459fab9ccf1bd510f3cd1239b473d85d74615ad
dayluxa <hadayaembara@gmail.com>
2025-03-28 23:18:42 +0700

    first commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'selamat menikmati... kalo ga nikmat maap hehehe'.split('')
+const text = 'selamat menikmati...                 kalo ga          nikmat maap hehehe'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
